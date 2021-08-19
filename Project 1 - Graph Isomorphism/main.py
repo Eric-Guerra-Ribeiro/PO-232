09ed1a716a8f60c93de65694ec00a9b70d1415f6
Eric Guerra Ribeiro <eric.guerra.07@gmail.com>
2021-08-19 10:34:21 -0300

    Add first example graphs.

--- Project 1 - Graph Isomorphism/main.py
+++ Project 1 - Graph Isomorphism/main.py
@@ -1,1 +1,15 @@
 import networkx as nx
+import matplotlib.pyplot as plt
+import isomorphismtest
+
+A = nx.Graph()
+A.add_nodes_from([1,2 ,3, 4, 5])
+A.add_edges_from([(1, 2), (1, 3), (1, 4), (2, 3), (3, 5), (4, 5)])
+B = nx.Graph()
+B.add_nodes_from("abcde")
+B.add_edges_from([("a", "b"), ("a", "c"), ("a", "e"), ("b", "c"), ("b", "d"), ("d", "e")])
+subax1 = plt.subplot(121)
+nx.draw(A, with_labels=True, font_weight='bold')
+subax2 = plt.subplot(122)
+nx.draw(B, with_labels=True, font_weight='bold')
+plt.show()
