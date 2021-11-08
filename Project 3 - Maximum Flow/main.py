8f7ccea088d7808f17fd024514a260d9f0b3dca3
Eric Guerra Ribeiro <eric.guerra.07@gmail.com>
2021-11-08 14:31:14 -0300

    Add new example

--- Project 3 - Maximum Flow/main.py
+++ Project 3 - Maximum Flow/main.py
@@ -39,6 +39,15 @@ C.add_nodes_from(["s", 1, 2, 3, 4, "t"])
 C.add_edges_from([("s", 1, {"max_flow" : 10}), ("s", 2, {"max_flow" : 10}), (1, 2, {"max_flow" : 2}),
                  (1, 3, {"max_flow" : 4}), (1, 4, {"max_flow" : 8}), (2, 4, {"max_flow" : 9}),
                  (3, "t", {"max_flow" : 10}), (4, 3, {"max_flow" : 6}), (4, "t", {"max_flow" : 10})])
+D = nx.DiGraph()
+D.add_nodes_from(["s", 0, 1, 2, 3, 4, 5, 6, 7, 8, "t"])
+D.add_edges_from([("s", 0, {"max_flow" : 7}), ("s", 1, {"max_flow" : 2}), ("s", 2, {"max_flow" : 1}),
+                 (0, 3, {"max_flow" : 2}), (0, 4, {"max_flow" : 4}), (1, 4, {"max_flow" : 5}),
+                 (1, 5, {"max_flow" : 6}), (2, 3, {"max_flow" : 4}), (2, 7, {"max_flow" : 8}),
+                 (3, 6, {"max_flow" : 7}), (3, 7, {"max_flow" : 1}), (4, 5, {"max_flow" : 2}),
+                 (4, 6, {"max_flow" : 3}), (4, 8 ,{"max_flow" : 3}), (5, 8, {"max_flow" : 3}),
+                 (6, "t", {"max_flow" : 1}), (7, "t", {"max_flow" : 3}), (8, "t", {"max_flow" : 4})])
 show_result(A)
 show_result(B)
 show_result(C)
+show_result(D)
